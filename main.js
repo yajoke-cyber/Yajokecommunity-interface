d7d23925a50fb32d057c6b7cf4a2252a2b6e5a93
Yajoke1024 <1065077075@qq.com>
2021-12-11 00:41:18 +0800

    跨域解决问题

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 const app = require("./app/index");
+const cors = require("koa2-cors");
 require("./app/database");
 const config = require("./app/config");
 console.log(process.cwd());
+app.use(
+  cors({
+    origin: function (ctx) {
+      return "*";
+      //设置允许来自指定域名请求
+      // if (ctx.url === "/test") {
+      //   return "*"; // 允许来自所有域名请求
+      // }
+      // return "http://localhost:8080"; //只允许http://localhost:8080这个域名的请求
+    },
+    maxAge: 5, //指定本次预检请求的有效期，单位为秒。
+    credentials: true, //是否允许发送Cookie
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], //设置所允许的HTTP请求方法'
+    allowHeaders: ["Content-Type", "Authorization", "Accept"], //设置服务器支持的所有头信息字段
+    exposeHeaders: ["WWW-Authenticate", "Server-Authorization"], //设置获取其他自定义字段
+  })
+);
 app.listen(config.APP_PORT, () => {
   console.log(`端口在${process.env.APP_PORT}打开了`);
 });
